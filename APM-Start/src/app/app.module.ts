2304f7bfc99bee52f448881b0f10a4ba5fa801b8
Jakub Bobak <jakub.bobak@capgemini.com>
2020-06-07 20:49:49 +0200

    Imports corrected.

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,9 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
-import {ProductListComponent} from './product/product-list/product-list.component';
+import {ProductListComponent} from './product/productListComponent/product-list.component';
 import {ConvertToSpacePipe} from './shared/convertToSpacePipeline/convert-to-space-pipe.pipe';
-import {StarComponent} from './shared/star/star.component';
+import {StarComponent} from './shared/starComponent/star.component';
 
 @NgModule({
   declarations: [
